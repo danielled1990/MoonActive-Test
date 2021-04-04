5d7f2af9cb53ae53ca52577f9895666488473a85
Danielle <danielled1990@gmail.com>
2021-04-04 18:06:18 +0300

    added fail

--- src/test/java/GetTextFromImageTest.java
+++ src/test/java/GetTextFromImageTest.java
@@ -1,4 +1,4 @@
-import StringForTests.StringForTets;
+import StringForTests.StringForTests;
 import com.google.common.collect.MapDifference;
 import com.google.common.collect.Maps;
 import com.google.gson.Gson;
@@ -12,20 +12,17 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 
-import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.Duration;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @DisplayName("Get Text From Image test")
 class GetTextFromImageTest {
@@ -42,58 +39,45 @@ class GetTextFromImageTest {
     void test_checkResponseCode() {
         int responseCode = 0;
         try {
-            responseCode = getTextFromImage.getResponseCode(StringForTets.IMAGE_TEST_1);
+            responseCode = getTextFromImage.getResponseCode(StringForTests.IMAGE_TEST_1);
         } catch (IOException e) {
-            e.printStackTrace();
+            fail(String.format("%s, %s %s", IOException.class.getName(), StringForTests.SOMETHING_WENT_WRONG, e.getMessage()));
         }
         assertEquals(200, responseCode);
     }
 
     @Test()
-    void test_checkTimeOut()  {
-        try {
-            HttpsURLConnection responseCode = getTextFromImage.getConnection(new URL(StringUtils.IMAGE_URL + "StringUtils.IMAGE_TEST_1"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assertTimeout(Duration.ofMillis(10000), () -> {
-            System.out.println("Time out is too long");
-        });
-    }
-
-    @Test()
     void test_checkURL() {
         try {
-            URL url = getTextFromImage.getUrl(StringForTets.IMAGE_TEST_1);
+            URL url = getTextFromImage.getUrl(StringForTests.IMAGE_TEST_1);
             Assert.assertThat(url.toString(), CoreMatchers.containsString("870a01303588957"));
             Assert.assertThat(url.toString(), CoreMatchers.containsString("apikey="));
-            Assert.assertThat(url.toString(), CoreMatchers.containsString(StringForTets.IMAGE_TEST_1));
+            Assert.assertThat(url.toString(), CoreMatchers.containsString(StringForTests.IMAGE_TEST_1));
         } catch (MalformedURLException e) {
-            //assertThat(me.getMessage(), containsString("whatever"));
-
+            fail(String.format("%s, %s %s", MalformedURLException.class.getName(), StringForTests.SOMETHING_WENT_WRONG, e.getMessage()));
         }
 
     }
+
     @Test
     void test_compareGoodJson() throws IOException {
-        getTextFromImage.getConnection(new URL(StringUtils.IMAGE_URL + StringForTets.IMAGE_TEST_2));
+        getTextFromImage.getConnection(new URL(StringUtils.IMAGE_URL + StringForTests.IMAGE_TEST_2));
         JSONObject actualObject = getTextFromImage.getJsonObject();
         JsonParser jsonParser = new JsonParser();
         JSONObject expectedJson = returnJson("correctJson.json");
         JsonElement jsonElementActual = jsonParser.parse(actualObject.toString());
         JsonElement jsonElementExpected = jsonParser.parse(expectedJson.toString());
         Map<String, MapDifference.ValueDifference<Object>> diffMap = getMapDiffrence(jsonElementActual, jsonElementExpected);
-        Assert.assertTrue(diffMap.size()==1||diffMap.size()==0); //check if only the ProcessingTimeInMilliseconds is different
+        Assert.assertTrue(diffMap.size() == 1 || diffMap.size() == 0); //check if only the ProcessingTimeInMilliseconds is different
     }
 
     @Test
     void test_compareErrorJson() throws IOException {
-        getTextFromImage.getConnection(new URL(StringUtils.IMAGE_URL + StringForTets.IMAGE_TEST_ERROR));
+        getTextFromImage.getConnection(new URL(StringUtils.IMAGE_URL + StringForTests.IMAGE_TEST_ERROR));
         JSONObject jsonObject = getTextFromImage.getJsonObject();
         JSONObject jsonObject1 = returnJson("errorJson.json");
-        assertEquals(jsonObject1.toString(),jsonObject.toString());
+        assertEquals(jsonObject1.toString(), jsonObject.toString());
     }
-
 
 
     private JSONObject parseJSONFile(String filename) throws JSONException, IOException {
@@ -102,15 +86,16 @@ class GetTextFromImageTest {
     }
 
     private JSONObject returnJson(String path) throws IOException, JSONException {
-        String filename = StringForTets.PATH + path;
+        String filename = StringForTests.PATH + path;
         JSONObject jsonObject = parseJSONFile(filename);
         return jsonObject;
     }
 
-    private Map<String, MapDifference.ValueDifference<Object>> getMapDiffrence(JsonElement jsonElement, JsonElement jsonElement1){
-        Type type = new TypeToken<Map<String, Object>>(){}.getType();
+    private Map<String, MapDifference.ValueDifference<Object>> getMapDiffrence(JsonElement jsonElement, JsonElement jsonElement1) {
+        Type type = new TypeToken<Map<String, Object>>() {
+        }.getType();
         Gson gson = new Gson();
-        Map<String, Object> leftMap = gson.fromJson(jsonElement,type);
+        Map<String, Object> leftMap = gson.fromJson(jsonElement, type);
         Map<String, Object> rightMap = gson.fromJson(jsonElement1, type);
         MapDifference<String, Object> difference = Maps.difference(leftMap, rightMap);
         Map<String, MapDifference.ValueDifference<Object>> difMap = difference.entriesDiffering();
--- src/test/java/StringForTests/StringForTests.java
+++ src/test/java/StringForTests/StringForTests.java
@@ -1,10 +1,11 @@
 package StringForTests;
 
-public class StringForTets {
+public class StringForTests {
 
     public static final String IMAGE_TEST_1 = "https://i.imgur.com/Atb2xiW.png";
     public static final String IMAGE_TEST_2 ="https://i.imgur.com/QYKhr34.gif";
     public static final String IMAGE_TEST_ERROR ="https://i.imgur.com/QYKhr3.gif";
     public static final String PATH =System.getProperty("user.dir")+"\\src\\test\\java\\";
 
+    public static final String SOMETHING_WENT_WRONG ="this exception was triggered, please see why";
 }
